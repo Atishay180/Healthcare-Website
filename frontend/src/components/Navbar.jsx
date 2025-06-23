3eada1e7fd09b731099e82a044e3b9c34b3737c3
Atishay180 <atishayjain8807@gmail.com>
2025-06-23 19:51:20 +0530

    feat: enhance mobile navbar with new icons and improved layout for better user experience

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,6 +5,9 @@ import { AppContext } from '../context/AppContext';
 import { AuthContext } from '../context/AuthContext';
 import AlertBox from './AlertBox';
 
+import { FaBars } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
+
 const Navbar = () => {
     const navigate = useNavigate();
     const { token, setToken, userData } = useContext(AppContext);
@@ -101,30 +104,55 @@ const Navbar = () => {
                             </div>
                     }
 
-                    <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+                    <button className='text-2xl text-gray-800 md:hidden' onClick={() => setShowMenu(true)}><FaBars /></button>
                     {/* -------------Mobile Menu------------- */}
-                    <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all duration-300`}>
+                    <div className={`${showMenu ? 'fixed w-40 sm:w-48' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-y-auto scroll-smooth flex flex-col justify-between rounded-l-lg shadow-xl shadow-gray-700 bg-white transition-all duration-300`}>
                         <div className='flex items-center justify-between px-5 py-6'>
-                            <img className='w-36' src={assets.logo} alt="" />
-                            <img className='w-7' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
+
+                            {/* logo  */}
+                            <div>
+                                <img className='w-12' src={assets.logo} alt="" />
+                                <p className='text-sm text-primary font-semibold'>Health<span className='text-tertiary'>Care</span></p>
+                            </div>
+
+                            {/* closing icon */}
+                            <button className='text-4xl text-gray-800' onClick={() => setShowMenu(false)}><RxCross2 /></button>
                         </div>
-                        <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+
+
+                        <ul className='flex flex-col items-start gap-6 mt-5 px-5 mb-16 text-sm font-medium'>
                             <NavLink onClick={() => setShowMenu(false)} to="/">
-                                <p className="px-4 py-2 rounded inline-block">HOME</p>
+                                <p className="px-2 py-1 rounded inline-block">HOME</p>
                             </NavLink>
 
                             <NavLink onClick={() => setShowMenu(false)} to="/doctors">
-                                <p className="px-4 py-2 rounded inline-block">ALL DOCTORS</p>
+                                <p className="px-2 py-1 rounded inline-block">ALL DOCTORS</p>
                             </NavLink>
 
                             <NavLink onClick={() => setShowMenu(false)} to="/about">
-                                <p className="px-4 py-2 rounded inline-block">ABOUT</p>
+                                <p className="px-2 py-1 rounded inline-block">ABOUT</p>
                             </NavLink>
 
                             <NavLink onClick={() => setShowMenu(false)} to="/contact">
-                                <p className="px-4 py-2 rounded inline-block">CONTACT</p>
+                                <p className="px-2 py-1 rounded inline-block">CONTACT</p>
                             </NavLink>
                         </ul>
+
+
+                        <div className='flex flex-col py-5 text-sm gap-2'>
+                            <button
+                                onClick={() => {navigate('/login'), setShowMenu(false)}}
+                                className='bg-primary text-white hover:scale-95 mx-4 transition-all duration-75 px-1 py-2 rounded-lg'
+                            >
+                                Create Account
+                            </button>
+                            <button
+                                onClick={() => (showConfirmation("You will be redirected to the admin login page. Are you sure you want to continue?", handleAdminLogin), setShowMenu(false))}
+                                className='text-primary border border-primary hover:scale-95 mx-4 transition-all duration-75 px-1 py-2 rounded-lg'
+                            >
+                                Admin Login
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
